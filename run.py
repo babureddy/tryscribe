889bc83859f36ee68c0b71f40729f71e8159acba
babu reddy <babureddy@rocketmail.com>
2017-12-09 10:48:33 +0530

    added recipients upload utility

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from werkzeug import secure_filename
 import os
  
 # App config.
@@ -7,14 +8,17 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-file_name = "c:\\Users\\LOKESH\\tryscribe\\campaign.csv"; 
+file_name = "c:\\Users\\LOKESH\\tryscribe\\campaign.csv";
+UPLOAD_FOLDER = 'c:\\Users\\LOKESH\\tryscribe\\uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
     stages = TextField('Stages:', validators=[validators.required()])
     schedule = TextField('Schedule:', validators=[validators.required()])
     subject = TextField('Subject:', validators=[validators.required()])
     body = TextField('Body:', validators=[validators.required()])
- 
+
 def save(message):
     file_object = open(file_name, 'a') 
     file_object.write(message + "\n")
@@ -25,10 +29,18 @@ def read():
         content = f.readlines()
     return content
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        flash('filename ' + file.filename)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "file uploaded successfully"
+
+@app.route("/hello", methods=['GET', 'POST'])
 def hello():
-    form = ReusableForm(request.form)
- 
+    form = ReusableForm(request.form) 
     print (form.errors)
     if request.method == 'POST':
         name=request.form['name']
@@ -46,6 +58,7 @@ def hello():
                 content = ""
                 for line in f.readlines():
                     flash(line)
+            return render_template('recipients.html', form=form)
         else:
             flash('All the form fields are required. ')
  
